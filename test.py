6981270277b1bacdfb2d9acfe25c8542255ecb57
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2024-11-27 20:32:22 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import pytest
+from main import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get("/")
+    assert response.data == b"Hello"
+    assert response.status_code == 200
